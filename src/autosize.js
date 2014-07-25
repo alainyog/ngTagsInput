457e834d320a1a887a6a7e8c9db09c35a7cc7878
Collin Henderson <hendo13@me.com>
2014-07-25 19:47:41 -0400

    Autosize fix for when tag field isnt immediately shown

--- src/autosize.js
+++ src/autosize.js
@@ -1,26 +1,26 @@
 'use strict';
 
 /**
- * @ngdoc directive
- * @name tiAutosize
- * @module ngTagsInput
- *
- * @description
- * Automatically sets the input's width so its content is always visible. Used internally by tagsInput directive.
- */
-tagsInput.directive('tiAutosize', function() {
+* @ngdoc directive
+* @name tiAutosize
+* @module ngTagsInput
+*
+* @description
+* Automatically sets the input's width so its content is always visible. Used internally by tagsInput directive.
+*/
+tagsInput.directive('tiAutosize', ['$timeout', function($timeout) {
     return {
         restrict: 'A',
         require: 'ngModel',
         link: function(scope, element, attrs, ctrl) {
             var THRESHOLD = 3,
-                span, resize;
+            span, resize;
 
             span = angular.element('<span class="input"></span>');
             span.css('display', 'none')
-                .css('visibility', 'hidden')
-                .css('width', 'auto')
-                .css('white-space', 'pre');
+            .css('visibility', 'hidden')
+            .css('width', 'auto')
+            .css('white-space', 'pre');
 
             element.parent().append(span);
 
@@ -51,6 +51,13 @@ tagsInput.directive('tiAutosize', function() {
                     resize(value);
                 }
             });
+            scope.$parent.$watch(element.parents('tags-input').attr('resize'), function (value) {
+                if(value){
+                    $timeout(function(){
+                        resize(ctrl.$modelValue);
+                    }, 0);
+                }
+            });
         }
     };
-});
+}]);
